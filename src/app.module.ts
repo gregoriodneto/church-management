bc6e5b2e6e82aa8e72a52578e2cc85314e5c8cb2
gregoriodneto <igregoriodoneto01@gmail.com>
2025-05-01 09:19:11 -0300

    feat: Criar Dockerfile e docker-compose.yml com PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChurchModule } from './modules/church/church.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { FinancesModule } from './modules/finances/finances.module';
+import { MembersModule } from './modules/members/members.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ChurchModule, 
+    DashboardModule, 
+    FinancesModule, 
+    MembersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
